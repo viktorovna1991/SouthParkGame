282fd814bbbd05226d5cef86ae878697c03c01b2
viktorovna1991 <viktorovna1991@gmail.com>
2020-03-28 18:40:27 +0300

    added css.style file

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // SELECT CVS
 const cvs = document.getElementById("game");
 const ctx = cvs.getContext("2d");
+ctx.scale(1,1);
 
 ctx.mozImageSmoothingEnabled = false;
 ctx.webkitImageSmoothingEnabled = false;
@@ -17,27 +18,28 @@ clouds.src = "images/clouds.jpg";
 const background = new Image();
 background.src = "images/bg.png";
 
-// BACKGROUND
-bg = {
-    sX: 0,
-    sY: 0,
-    w: 1366,
-    h: 768,
-    x: 0,
-    y: 0,
+// // BACKGROUND
+// bg = {
+//     sX: 0,
+//     sY: 0,
+//     w: 1366,
+//     h: 768,
+//     x: 0,
+//     y: 0,
+//
+//     draw: function () {
+//         ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w * 0.17, this.h * 0.17);
+//     }
+//
+// };
 
-    draw: function () {
-        ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w * 0.17, this.h * 0.17);
-    }
-
-};
 
 // DRAW
 function draw() {
-    ctx.fillStyle = "#70c5ce";
-    ctx.fillRect(0, 0, cvs.width, cvs.height);
+    // ctx.fillStyle = "#70c5ce";
+    // ctx.fillRect(0, 0, cvs.width, cvs.height);
 
-    bg.draw();
+    // bg.draw();
 }
 
 // UPDATE
