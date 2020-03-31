66ab93b790dd4e1fa8875e22abbf470248770f3b
viktorovna1991 <viktorovna1991@gmail.com>
2020-03-31 19:22:58 +0300

    fixed canvas quality

--- game.js
+++ game.js
@@ -7,12 +7,34 @@ ctx.webkitImageSmoothingEnabled = false;
 ctx.msImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
 
+function setupCanvas(canvas) {
+    // Get the device pixel ratio, falling back to 1.
+    var dpr = window.devicePixelRatio || 1;
+    // Get the size of the canvas in CSS pixels.
+    var rect = canvas.getBoundingClientRect();
+    // Give the canvas pixel dimensions of their CSS
+    // size * the device pixel ratio.
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+    var ctx = canvas.getContext('2d');
+    // Scale all drawing operations by the dpr, so you
+    // don't have to worry about the difference.
+    ctx.scale(dpr, dpr);
+    return ctx;
+}
+
+var ct = setupCanvas(document.getElementById('game'));
+
+
 // GAME VARS AND CONSTS
 let frames = 0;
 
 // LOAD SPRITE IMAGE
 const clouds = new Image();
 clouds.src = "images/clouds.jpg";
+
+const cartmans = new Image();
+cartmans.src = "images/cartman.png";
 
 var wrapper = document.getElementById("wrapper");
 var background = document.createElement('img');
@@ -44,16 +66,39 @@ bg = {
     },
 
     update: function () {
-        this.x = (this.x - this.dx)%1365;
-        console.log('x', this.x);
-
-
+        this.x = (this.x - this.dx) % 1365;
     }
+};
+
+// CARTMAN
+const cartman = {
+    animation: [
+        {sX: 461, sY: 54},
+        {sX: 276, sY: 139},
+        {sX: 276, sY: 164},
+        {sX: 276, sY: 139}
+    ],
+    x: 150,
+    y: 450,
+    w: 133,
+    h: 174,
+    dw: 133,
+    dh: 174,
+
+    frame: 0,
+
+    draw: function () {
+        let cartman = this.animation[this.frame];
+
+        ctx.drawImage(cartmans, cartman.sX, cartman.sY, this.w, this.h, parseInt(this.x)+ 0.5, parseInt(this.y)+ 0.5, this.dw, this.dh);
+    },
+
 };
 
 // DRAW
 function draw() {
     bg.draw();
+    cartman.draw();
 }
 
 // UPDATE
